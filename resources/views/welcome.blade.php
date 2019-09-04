0050751a740e7e67cb86c6d96239ab4b12161300
GarciaAlejandro <alejandrogc672@gmail.com>
2019-09-03 20:20:06 -0500

    Second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #f00;
+                background-color: #000;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
