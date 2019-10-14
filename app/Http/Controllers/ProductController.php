99d0b862edbd57c16b6dac740aabbbcefae4e9e8
GarciaAlejandro <alejandrogc672@gmail.com>
2019-10-13 22:34:00 -0500

    Validation errors [SH]

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\ProductRequest;
+use \Illuminate\Validation\Validator;
 
 class ProductController extends Controller
 {
@@ -18,7 +21,7 @@ class ProductController extends Controller
         $products = Product::all();
         // Se dan los productos en un JSON con el código 200
         return response()->json($products, 200);
-
+         
     }
 
     /**
@@ -37,15 +40,16 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         // Create a new product
         $product = Product::create($request->all());
 
         // Return a response with a product json
         // representation and a 201 status code   
+      
         return response()->json($product,201);
-    }
+    } 
 
     /**
      * Display the specified resource.
@@ -56,11 +60,11 @@ class ProductController extends Controller
     public function show($id)
     {
         // Se busca el producto en tabla 
-        $product = Product::find($id);
+        $product = Product::findOrFail($id);
 
         // Se retorna el producto solicitado, con el status 200 (OK)
         return response()->json($product,200);
-
+    
     }
 
     /**
@@ -71,8 +75,6 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-
-
     }
 
     /**
@@ -82,13 +84,13 @@ class ProductController extends Controller
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
-        //
         // Se busca el producto en la tabla
-        $product = Product::find($id);
+        $product = Product::findOrFail($id);
         // Se guarda el producto actualizado
         $product->update($request->all());
+        
         // Se retorna el producto modificado, con el status 200 (OK)
         return response()->json($product,200);
     }
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -7,6 +7,9 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illumintae\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
+
 
 class ProductTest extends TestCase
 {
@@ -18,19 +21,24 @@ class ProductTest extends TestCase
 
     use RefreshDatabase;
 
+    /**
+     *      CREATE-1 
+     */
     public function test_client_can_create_a_product()
     {
         // GIVEN
+        // El Cliente Tiene tiene una representación de un producto que quiere agregar a la aplicación
         $productData = [
             'name' => 'Super Product',
             'price' => '130'
         ];
 
         // When
+        // Call to the EndPoint to create a product
         $response = $this->json('POST', '/api/products', $productData);
 
         // Then
-            // Assert it sends the correct HTTP Status
+        // Assert it sends the correct HTTP Status
         $response->assertStatus(201);
 
         // Assert the response has the correct structure
@@ -61,37 +69,269 @@ class ProductTest extends TestCase
         
     }
 
-    // LIST ALL PRODUCTS
-    // OK
-    public function test_client_can_list_all_products(){
+    /**
+     *      CREATE-2
+     */
+    public function test_client_dont_send_name(){
         // GIVEN
-        // Verificar contenido de la base de datos
-        // The products already created
-        $listProduct = factory(Product::class, 10)->create();
-        // WHEN
-        // I send a request for all products
-        $response = $this->json('GET','/api/products');
-        // THEN 
-        // STATUS CODE 200
-        $response->assertStatus(200);
+        // El cliente no envía el atributo "name" en el cuerpo de la solicitud     
+        $productData = [
+            'name' => '',
+            'price' => '10'
+        ];
 
 
-        // DESPLEGAR DATOS 
-        //  A list with all the products representations are returned
-        $response->assertJsonStructure([
-              '*' => [
-                'id',
-                'name',
-                'price',
-                'created_at',
-                'updated_at',
-              ]
-          ]);
+         // When
+         $response = $this->json('POST', '/api/products', $productData);
+
+        // Then 
+        // Assert the response has the correct format
+        $response->assertJson([
+                "errors" => [[
+                    "code" => "Error-1",
+                    "title" => "Unprocessable Entity",
+                ]]
+        ]);
+
+        // Assert the Status HTTP
+        $response->assertStatus(422);
     }
 
-    // SHOW PRODUCT
+    /**
+     *      CREATE-3
+     */
+    public function test_client_dont_send_price(){
+         // GIVEN
+        // El cliente no envía el atributo "PRICE" en el cuerpo de la solicitud     
+        $productData = [
+            'name' => 'Botella',
+            'price' => ''
+        ];
+
+        // When
+        $response = $this->json('POST', '/api/products', $productData);
+        
+    
+        // Se verifica el JSON de respuesta 
+        $response->assertJson([
+                "errors" => [[
+                    "code" => "Error-1",
+                    "title" => "Unprocessable Entity",
+                ]]
+        ]);
+        
+        // Se verifica el código HTTP
+        $response->assertStatus(422);
+
+    }
+    /**
+     *      CREATE-4
+     */
+    public function test_client_send_price_not_number(){
+        // GIVEN
+        // El cliente envía la representación de un producto donde el atributo "price" NO es un número   
+        $productData = [
+            'name' => 'Botella',
+            'price' => 'NoNumero'
+        ];
+
+        // When
+        $response = $this->json('POST', '/api/products', $productData);
+
+        // Se verifica el JSON de respuesta 
+        $response->assertJson([
+            "errors" => [[
+                "code" => "Error-1",
+                "title" => "Unprocessable Entity",
+            ]]
+        ]);
+        
+        // Se verifica el código HTTP
+        $response->assertStatus(422);
+    }
+
+    /**
+     *      CREATE-5
+     */
+    public function test_client_send_price_negative(){
+        // GIVEN
+        // El cliente envía la representación de un producto donde el atributo "price" tiene un valor de "-20"   
+        $productData = [
+            'name' => 'Botella',
+            'price' => '-20'
+        ];
+
+        // When
+        $response = $this->json('POST', '/api/products', $productData);
+
+            // Se verifica el JSON de respuesta 
+            $response->assertJson([
+                "errors" => [[
+                    "code" => "Error-1",
+                    "title" => "Unprocessable Entity",
+                ]]
+        ]);
+        
+        // Se verifica el código HTTP
+        $response->assertStatus(422);
+    }
+
+    /**
+     *      UPDATE-1
+     */
+    public function test_client_can_update_a_product(){
+        
+        // GIVEN
+        // A product to edit with ID (Factory)
+        $productData = factory(Product::class)->create([
+            "name" => "Text",
+            "price" => "10"
+        ]);
+
+        // The body of the request with the values edited
+        $productDataUpdate = [
+            "name" => "Text Edited",
+            "price" => "20"
+        ];
+        // WHEN
+        // I send a request to update the product
+        $response = $this->json('PUT',"api/products/".$productData->id, $productDataUpdate);
+        
+        // THEN 
+        // Return 200 Status
+        
+        $response->assertStatus(200);
+
+        $body = $response->decodeResponseJson();
+    
+        // Assert the response has the correct structure
+        $response->assertJsonStructure([
+            'id',
+            'name',
+            'price'
+        ]);
+
+        // Assert the product was edited
+        // with the correct data
+    
+        $response->assertJson([
+            'name' => 'Text Edited',
+            'price' => '20'
+        ]);
+        
+        // Assert the product Edited there is in the DB
+         $this->assertDatabaseHas('products', $productDataUpdate);
+    }
+
+    /** 
+     *      UPDATE-2
+     */ 
+    public function test_client_send_price_not_number_update(){
+        // Se crea una instancia de producto en la base de datos
+        $productData = factory(Product::class)->create([
+            "name" => "Text",
+            "price" => "10"
+        ]);  
+
+        // GIVEN
+        // El cliente envía la representación de un producto donde el atributo "PRICE" No es un número válido
+        $productDataUpdate = [
+            "name" => "Text Edited",
+            "price" => "NotNumber"
+        ];
+
+        // WHEN
+        // I send a request to update the product
+        $response = $this->json('PUT',"api/products/".$productData->id, $productDataUpdate);
+
+        // Assert the response has the correct structure
+        $response->assertJson([
+            "errors" => [[
+                "code" => "Error-1",
+                "title" => "Unprocessable Entity"
+            ]]
+        ]);
+        
+        // Assert the Status HTTP
+        $response->assertStatus(422);
+    }
+
+    /**
+     *      UPDATE-3
+     */
+    
+    public function test_client_send_price_negative_update(){
+        // Se crea una instancia de producto en la base de datos
+        $productData = factory(Product::class)->create([
+        "name" => "Text",
+        "price" => "10"
+        ]);  
+
+        // GIVEN
+        // El cliente envía la representación de un producto donde el atributo "PRICE" No es un número válido
+        $productDataUpdate = [
+            "name" => "Text Edited",
+            "price" => "-20"
+        ];
+
+        // WHEN
+        // I send a request to update the product
+        $response = $this->json('PUT',"api/products/".$productData->id, $productDataUpdate);
+
+        // Assert the response has the correct structure
+        $response->assertJson([
+            "errors" => [[
+                "code" => "Error-1",
+                "title" => "Unprocessable Entity"
+            ]]
+        ]);
+
+        // Assert the Status HTTP
+        $response->assertStatus(422);
+
+    }
+
+    /**
+     *      UPDATE-4
+     */
+    public function test_client_send_id_invalid(){
+        //GIVEN 
+        // EL cliente manda una solicitud solicitando editar un producto con un ID que no existe
+        $productData = factory(Product::class)->create([
+            "name" => "Text",
+            "price" => "10"
+        ]);  
+
+        // Representación del producto a editar 
+        $productDataUpdate =[
+            "name" => "Text Edited",
+            "price" => "10",
+        ];
+
+        // WHEN
+        // I send a request to update the product
+        // ID inválido, debido a que la base de datos se limpia en cada prueba
+        // id = id + 1 
+        $response = $this->json('PUT',"api/products/".($productData->id+1), $productDataUpdate);
+
+        // Assert the response has the correct structure
+        $response->assertJson([
+            "errors" => [[
+                "code" => "ERROR-2",
+                "title" => "Not Found"
+            ]]
+        ]);
+
+        // Assert Status HTTP
+        $response->assertStatus(404);
+
+    }
+
+    /**
+     *      SHOW-1
+     */
     public function test_client_can_show_a_product(){
-   
+    
         // GIVEN
         // There is a product in the application
         $productData = factory(Product::class)->create();
@@ -119,49 +359,39 @@ class ProductTest extends TestCase
             'price' => $productData->price,
         ]);
 
-        
-     
-
     }
-    
-    // UPDATE PRODUCT
-    public function test_client_can_update_a_product(){
-        
+
+    /**
+     *      SHOW-2
+     */
+    public function test_client_try_show_a_product_with_id_invalid(){
         // GIVEN
-        // A product to edit with ID (Factory)
-        $productDataUpdate = factory(Product::class)->create([
-            'name' => 'Text EDITED',
-            'price' => '10'
-        ]);
+        // El cliente manda una solicitud solicitando un producto con un ID que no existe
+        // There is a product in the application
+        $productData = factory(Product::class)->create();
+
         // WHEN
-        // I send a request to update the product
-        $response = $this->json('PUT',"api/products/".$productDataUpdate->id);
-
-        // THEN 
-        // Return 200 Status
+        // I send a request for the product 
+        $response = $this->json("GET","/api/products/".($productData->id+1) );
         
-        $response->assertStatus(200);
+        // THEN 
+        // A 404 Status code is returned
+        $response->assertStatus(404);
 
-        $body = $response->decodeResponseJson();
-       
         // Assert the response has the correct structure
-        $response->assertJsonStructure([
-            'id',
-            'name',
-            'price'
+        $response->assertJson([
+            "errors" => [[
+                "code" => "ERROR-2",
+                "title" => "Not Found"
+            ]]
         ]);
 
-        // Assert the product was edited
-        // with the correct data
-       
-        $response->assertJson([
-            'name' => 'Text EDITED',
-            'price' => '10'
-        ]);
 
     }
 
-    // DELETE PRODUCT
+    /**
+     *      DELETE-1
+     */
     public function test_client_can_delete_a_product(){
 
         // GIVEN
@@ -175,7 +405,90 @@ class ProductTest extends TestCase
         // THEN 
         // A 204 status code is returned
         $response->assertStatus(204);
+
+        // Assert that the given string is contained within the response
+        // Body empty
+        $response->assertSee(null);
     }
+
+    /**
+     *      DELETE-2
+     */
+
+    public function test_client_try_delete_a_product_with_invalid_id(){
+        // GIVEN
+        //There is a product in the application
+        $product = factory(Product::class)->create();
+
+        // WHEN
+        // El cliente manda una solicitud solicitando eliminar un producto con un ID que no existe
+        // I send a request to delete the product
+        $response = $this->delete('/api/products/'.($product->id+1));
+       
+        // THEN 
+        // A 404 status code is returned
+        $response->assertStatus(404); 
+
+        // Assert the response has the correct structure
+        $response->assertJson([
+            "errors" => [[
+                "code" => "ERROR-2",
+                "title" => "Not Found"
+            ]]
+        ]);
+
+    }
+
+    /**
+     *      LIST-1
+     */
+    public function test_client_can_list_all_products(){
+        // GIVEN
+        // Verificar contenido de la base de datos
+        // The products already created
+        $listProduct = factory(Product::class, 2)->create();
+        // WHEN
+        // I send a request for all products
+        $response = $this->json('GET','/api/products');
+        // THEN 
+        // STATUS CODE 200
+        $response->assertStatus(200);
+
+
+        // DESPLEGAR DATOS 
+        //  A list with all the products representations are returned
+        $response->assertJsonStructure([
+              '*' => [
+                'id',
+                'name',
+                'price',
+              ]
+          ]);
+    }
+    /**
+     *      LIST-2
+     */
+    public function test_client_can_list_empty(){
+        // GIVEN
+        // List is empty
+
+        // WHEN
+        // I send a request for all products
+        $response = $this->json('GET','/api/products');
+
+        // THEN 
+        // STATUS CODE 200
+        $response->assertStatus(200);
+
+           // DESPLEGAR DATOS 
+        //  A list with all the products representations are returned
+        $response->assertJsonStructure([]);
+
+    }
+    
+  
+
+
     
     
 }
